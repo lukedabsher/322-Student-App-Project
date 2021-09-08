8290770f12f159b1f1cd962ffda3ef69454f2c3c
Luke <lucas.absher@wsu.edu>
2021-09-07 17:57:42 -0700

    Version v.08 Revised database model. Created a many to many relationship 'enrolled' between Student and Class.

--- app/models.py
+++ app/models.py
@@ -9,12 +9,20 @@ from app import login
 def load_user(id):
     return Student.query.get(int(id))
     
+enrolled = db.Table('enrolled',
+db.Column('studentid', db.Integer, db.ForeignKey('student.id')),
+db.Column('classid', db.Integer, db.ForeignKey('class.id'))
+)
 
 class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     coursenum = db.Column(db.String(3))  
     title = db.Column(db.String(150))
     major = db.Column(db.String(20),db.ForeignKey('major.name'))
+    roster = db.relationship(
+        'Student' , secondary= enrolled,
+        primaryjoin=(enrolled.c.classid == id), lazy = 'dynamic', overlaps="classes")
+    
     def __repr__(self):
         return '<Class id: {} - coursenum: {}, title {}, major: {}>'.format(self.id,self.coursenum,self.title,self.major)
     def getTitle(self):
@@ -36,7 +44,10 @@ class Student(UserMixin,db.Model):
     address = db.Column(db.String(128))
     email =  db.Column(db.String(128),unique=True,index=True)
     last_seen =  db.Column(db.DateTime, default=datetime.utcnow)
-
+    classes = db.relationship(
+        'Class' , secondary= enrolled,
+        primaryjoin=(enrolled.c.studentid == id), lazy = 'dynamic', overlaps="roster")
+    
     def __repr__(self):
         return '<Student {} - {}{} - {};>'.format(self.id,self.firstname, self.lastname, self.email)
 
@@ -46,4 +57,16 @@ class Student(UserMixin,db.Model):
     def check_password(self, password):
         self.password_hash = check_password_hash(password)
 
-    
+    def enroll(self,newclass):
+        if not self.is_enrolled(newclass):
+            self.classes.append(newclass)
+
+    def unenroll(self,oldclass):
+        if self.is_enrolled(oldclass):
+            self.classes.remove(oldclass)
+
+    def is_enrolled(self,newclass):
+        self.classes.filter(enrolled.c.classid == newclass.id).count() > 0
+
+    def enrolledCourses(self):
+        return self.classes
